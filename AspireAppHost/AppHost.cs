85c34ecfc4ca08a186b7be79ff568420488cd491
David Gardiner <david@gardinerfamily.au>
2025-10-26 18:07:16 +1030

    OpenTelemetry working

--- AspireAppHost/AppHost.cs
+++ AspireAppHost/AppHost.cs
@@ -1,10 +1,21 @@
+using AspireAppHost;
+using Microsoft.Extensions.Hosting;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddNpmApp("nodeapp", "../NodeApp")
-    .WithHttpEndpoint(env: "PORT");
+var nodeApp = builder.AddPnpmApp("pnpapp", "../NodeApp")
+    // If you are using fnm for Node.js version management, you might need to adjust the PATH
+    .WithEnvironment("PATH", Environment.GetEnvironmentVariable("PATH") + ";" + Environment.ExpandEnvironmentVariables(@"%USERPROFILE%\AppData\Roaming\fnm\aliases\default"))
 
-builder.AddPnpmApp("pnpapp", "../NodeApp")
     .WithHttpEndpoint(env: "PORT")
+    .WithOtlpExporter()
     .WithHttpHealthCheck("/");
+
+var launchProfile = builder.Configuration["DOTNET_LAUNCH_PROFILE"];
+
+if (builder.Environment.IsDevelopment() && launchProfile == "https")
+{
+    nodeApp.RunWithHttpsDevCertificate("HTTPS_CERT_FILE", "HTTPS_CERT_KEY_FILE");
+}
 
 builder.Build().Run();
