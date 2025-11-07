de5c54eefaf60d1dd9c2dd967e5a2e7eb3b3f7e8
David Gardiner <david@gardinerfamily.au>
2025-11-07 13:54:40 +1030

    Update to 13 daily and use Aspire.Hosting.JavaScript

--- AspireAppHost/AppHost.cs
+++ AspireAppHost/AppHost.cs
@@ -1,21 +1,13 @@
-using AspireAppHost;
-using Microsoft.Extensions.Hosting;
-
 var builder = DistributedApplication.CreateBuilder(args);
 
-var nodeApp = builder.AddPnpmApp("pnpapp", "../NodeApp")
+var nodeApp = builder.AddJavaScriptApp("pnpapp", "../NodeApp")
+    .WithPnpm()
+    .WithRunScript("start")
     // If you are using fnm for Node.js version management, you might need to adjust the PATH
     .WithEnvironment("PATH", Environment.GetEnvironmentVariable("PATH") + ";" + Environment.ExpandEnvironmentVariables(@"%USERPROFILE%\AppData\Roaming\fnm\aliases\default"))
 
     .WithHttpEndpoint(env: "PORT")
     .WithOtlpExporter()
     .WithHttpHealthCheck("/");
-
-var launchProfile = builder.Configuration["DOTNET_LAUNCH_PROFILE"];
-
-if (builder.Environment.IsDevelopment() && launchProfile == "https")
-{
-    nodeApp.RunWithHttpsDevCertificate("HTTPS_CERT_FILE", "HTTPS_CERT_KEY_FILE");
-}
 
 builder.Build().Run();
